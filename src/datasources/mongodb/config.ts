f6e3ef2dd223859a4008876caa9e64ed08bc27f4
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2020-09-11 11:17:45 +0200

    2020.09.11 2

--- src/datasources/mongodb/config.ts
+++ src/datasources/mongodb/config.ts
@@ -2,9 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
-const MongoDBConfig = {
-  url: process.env.MONGODB_URL ?? 'that will not work',
+export const MongoDBConfig = {
+  url: process.env.MONGODB_CONNECTION_STRING ?? 'that will not work',
   database: process.env.MONGODB_DB ?? 'that will not work',
 };
-
-export { MongoDBConfig };
